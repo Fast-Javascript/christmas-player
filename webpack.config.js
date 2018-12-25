988a633241c529ec9181e2e589226e536b8d0923
uchang.fd <uchang.fd@gmail.com>
2018-12-25 20:49:39 +0900

    [환경설정] add prettier trailingComma

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/",
   output: {
     path: path.resolve("dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -13,25 +13,25 @@ module.exports = {
         test: /\.js$/,
         exclude: [/node_modules/],
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(mp4|mp3|png|jpg|gif|jpeg)$/,
         use: {
-          loader: "file-loader"
-        }
-      }
-    ]
+          loader: "file-loader",
+        },
+      },
+    ],
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+  ],
 };
